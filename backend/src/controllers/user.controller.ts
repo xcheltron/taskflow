47884e2b1868a035ab3373bacf82e991e2447810
Xchel Uriel Pérez González <L20290857@cdguzman.tecnm.mx>
2026-07-24 11:30:11 -0600

    se agrego el types de bcryp y el hash para contraseñas

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -2,12 +2,23 @@
 import { createUserModel } from "../models/user.model.js";
 import type { Request, Response } from "express";
 
+import bcrypt from "bcrypt"
+import "dotenv/config"
+
+const SALT_ROUNDS = Number(process.env.SALT_ROUNDS)
+
+async function hasshedPassword(password:string):Promise<string> {
+    return await bcrypt.hash(password, SALT_ROUNDS)
+}
+
 export const registerUser = async (
     req: Request,
     res: Response ) =>{
     const { email, password, name} = req.body;
     try {
-        const result = await createUserModel(email, password, name)
+        //encriptar contraseña
+        const newPassword = await hasshedPassword(password)
+        const result = await createUserModel(email, newPassword, name)
         console.log(result)
         res.json(result)
     } catch (error) {
