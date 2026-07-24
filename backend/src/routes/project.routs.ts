41b4cdc99cc5739a9df1c467ab9d4ac34042c0aa
Xchel Uriel Pérez González <L20290857@cdguzman.tecnm.mx>
2026-07-24 15:04:36 -0600

    se agrego la ruta para la consulta general de proyectos

--- backend/src/routes/project.routs.ts
+++ backend/src/routes/project.routs.ts
@@ -1,9 +1,12 @@
 import { projectCreateController } from "../controllers/project.controller.js";
+import { projectSearchController } from "../controllers/project.controller.js";
 
 import { Router } from "express";
 
 const router = Router();
 
 router.post('/create', projectCreateController)
+router.get('/user/:id', projectSearchController)
+
 
 export default router;
