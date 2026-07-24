f44c8865b110e2bd640796b94e00ae87663d6131
Xchel Uriel Pérez González <L20290857@cdguzman.tecnm.mx>
2026-07-24 11:48:28 -0600

    controlador actualizado para login user

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -11,6 +11,10 @@ async function hasshedPassword(password:string):Promise<string> {
     return await bcrypt.hash(password, SALT_ROUNDS)
 }
 
+async function comparePassword(password:string, passwordhash:string):Promise<boolean> {
+    return bcrypt.compare(password, passwordhash)
+}
+
 export const registerUser = async (
     req: Request,
     res: Response ) =>{
@@ -27,5 +31,36 @@ export const registerUser = async (
         res.status(500).json({
             message: "Internal server error"
         })
+    }
+}
+
+export const loginUser = async (
+    req: Request,
+    res: Response
+) => {
+    const {email, password} = req.body
+    try {
+        //llamaos al modelo de usario para hacer una consulta del email y la contraseña
+        const user = await loginUserModel(email)
+
+        if (user == null) {
+            return res.status(404).json(
+                {message: "Usuario no encontrado"}
+            )
+        }
+        //llamamos a un metodo que verifique la contraseña
+        const isvalid = await comparePassword(password, user.password)
+
+        if (!isvalid) {
+            return res.status(401).json({
+                message: "contraseña incorrecta"
+            })}
+
+        return res.status(200).json({
+            message: "Usuario encontrado",
+            usuario: user
+        })
+    } catch (error) {
+        return res.status(500).json({message: "Error interno del servidor"})
     }
 }
