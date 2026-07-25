b7fc6384578838d4249184a8efa0552048d95522
Xchel Uriel Pérez González <L20290857@cdguzman.tecnm.mx>
2026-07-24 21:17:54 -0600

    se agrego la ruta para eliminar proyectos

--- backend/src/routes/project.routs.ts
+++ backend/src/routes/project.routs.ts
@@ -1,6 +1,7 @@
 import { projectCreateController } from "../controllers/project.controller.js";
 import { projectSearchController } from "../controllers/project.controller.js";
 import { projectUpdateController } from "../controllers/project.controller.js";
+import { projectDeleteController } from "../controllers/project.controller.js";
 
 import { Router } from "express";
 
@@ -9,6 +10,6 @@ const router = Router();
 router.post('/create', projectCreateController)
 router.get('/user/:id', projectSearchController)
 router.put('/update/:id', projectUpdateController)
-
+router.delete('/delete/:id', projectDeleteController)
 
 export default router;
