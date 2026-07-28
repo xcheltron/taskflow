7cc1e5b6a677cfe6cb3f5c50d33f3c456c8ff259
Xchel Uriel Pérez González <L20290857@cdguzman.tecnm.mx>
2026-07-28 12:40:25 -0600

    Prueba que se muestra login correctamente

---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,11 @@
+function Login() {
+    return (
+        <div className="min-h-screen bg-slate-900 flex items-center justify-center">
+            <h1 className="text-5xl font-bold text-white">
+                Login
+            </h1>
+        </div>
+    )
+}
+
+export default Login
