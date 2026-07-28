50476d069db8c30a9ee37e3116d9bbbe54bf8997
Xchel Uriel Pérez González <L20290857@cdguzman.tecnm.mx>
2026-07-28 12:39:42 -0600

    Se cambio la estructura de navegacion entre paginas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,9 @@
+import AppRouter from './routes/AppRouter'
+
 function App() {
 
-  return (
-    <div className="min-h-screen bg-slate-900 flex items-center justify-center">
-      <h1 className="text-5xl font-bold text-white">
-        TaskFlow
-      </h1>
-    </div>
-  )
+  return <AppRouter/>
+  
 }
 
 export default App
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from "react-router-dom"
+import { StrictMode } from 'react'
+
+
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
+  </StrictMode>
 )
