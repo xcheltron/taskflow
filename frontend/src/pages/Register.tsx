c572b3ae2fdc8621e5fc14ac21778943fe64bdc8
Xchel Uriel Pérez González <L20290857@cdguzman.tecnm.mx>
2026-07-28 12:40:38 -0600

    Prueba que se muestra Register correctamente

---
+++ frontend/src/pages/Register.tsx
@@ -0,0 +1,11 @@
+function Register() {
+    return (
+        <div className="min-h-screen bg-slate-900 flex items-center justify-center">
+            <h1 className="text-5xl font-bold text-white">
+                Register
+            </h1>
+        </div>
+    )
+}
+
+export default Register
