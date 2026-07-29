f6c9a0a263ed09fa41535a89815a5af0bfb1ef17
Xchel Uriel Pérez González <L20290857@cdguzman.tecnm.mx>
2026-07-29 12:34:14 -0600

    Se agrego la ruta de proyectos

--- frontend/src/routes/AppRouter.tsx
+++ frontend/src/routes/AppRouter.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import Home from "../pages/Home";
+import Projects from "../pages/Projects";
 
 function AppRouter () {
     return (
@@ -9,6 +10,7 @@ function AppRouter () {
             <Route path='/' element={<Login/>}/>
             <Route path='/register' element={<Register/>}/>
             <Route path="/home" element={<Home/>}/>
+            <Route path="/projects" element={<Projects/>}/>
         </Routes>
     )
 }
