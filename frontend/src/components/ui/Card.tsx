a1f34773870138e45baa06bc1577b78d75ac7cbd
Xchel Uriel Pérez González <L20290857@cdguzman.tecnm.mx>
2026-07-29 13:24:49 -0600

    update: se actualizao el componente de la carta

--- frontend/src/components/ui/Card.tsx
+++ frontend/src/components/ui/Card.tsx
@@ -3,11 +3,13 @@ import Button from "./Button"
 export interface propsCardLayout{
     title: string
     content: string
+    butonColor?: "ok" | "disable" | "delete"
+    textButon?: string
 }
 
 function Card(props: propsCardLayout) {
     return (
-        <div className="w-72 h-60 p-6 border rounded-md shadow-xs flex flex-col justify-between">
+        <div className="w-72 h-60 p-6 border rounded-md shadow-xs flex flex-col justify-between hover:cursor-pointer hover:scale-105 transition-transform">
             <div>
                 <h5 className="mb-3 text-2xl font-semibold">
                     {props.title}
@@ -17,10 +19,11 @@ function Card(props: propsCardLayout) {
                     {props.content}
                 </p>
             </div>
-
-            <Button type="submit" color="primary">
-                See Project
-            </Button>
+            {props.textButon && props.butonColor &&(            
+                <Button type="submit" color={props.butonColor}>
+                    {props.textButon}
+                </Button>
+            )}
         </div>
     );
 }
