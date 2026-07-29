5f4006323cca305d00225f0ccaa3c38d43f2e850
Xchel Uriel Pérez González <L20290857@cdguzman.tecnm.mx>
2026-07-29 12:34:39 -0600

    se agrego la pagina que renderiza los proyectos

---
+++ frontend/src/pages/Projects.tsx
@@ -0,0 +1,12 @@
+import ProjectForm from "../features/auth/components/ProjectForm";
+import MainLayout from "../layouts/MainLayout";
+
+function Projects () {
+    return (
+        <MainLayout>
+            <ProjectForm/>
+        </MainLayout>
+    )
+}
+
+export default Projects
