33752f4ab4969f1d0b298b4535e0872ad62a59d2
Xchel Uriel Pérez González <L20290857@cdguzman.tecnm.mx>
2026-07-29 15:10:36 -0600

    update: se agrego la ruta para acceder a tareas

--- frontend/src/routes/AppRouter.tsx
+++ frontend/src/routes/AppRouter.tsx
@@ -3,6 +3,7 @@ import Login from "../pages/Login";
 import Register from "../pages/Register";
 import Home from "../pages/Home";
 import Projects from "../pages/Projects";
+import Tasks from "../pages/Tasks"
 
 function AppRouter () {
     return (
@@ -11,6 +12,7 @@ function AppRouter () {
             <Route path='/register' element={<Register/>}/>
             <Route path="/home" element={<Home/>}/>
             <Route path="/projects" element={<Projects/>}/>
+            <Route path="/tasks" element={<Tasks/>}/>
         </Routes>
     )
 }
