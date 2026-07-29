02e4df3713ad7a638a29f61db27e8b598274b174
Xchel Uriel Pérez González <L20290857@cdguzman.tecnm.mx>
2026-07-29 13:24:38 -0600

    update: se actualizao el componente del boton

--- frontend/src/components/ui/Button.tsx
+++ frontend/src/components/ui/Button.tsx
@@ -1,24 +1,25 @@
 export interface ButtonProps {
     children: React.ReactNode
     type?: "button" | "submit" | "reset"
-    color?: "primary" | "secondary"
+    color?: "ok" | "disable" | "delete"
 }
 
 function Button({
     children,
     type = "button",
-    color = "primary"
+    color = "ok"
 }: ButtonProps) {
 
     const styles = {
-        primary: "bg-blue-600 text-white",
-        secondary: "bg-gray-200 text-black"
+        ok: "bg-blue-600 text-white",
+        disable: "bg-gray-200 text-black",
+        delete: "bg-red-600 text-white"
     }
 
     return (
         <button 
             type={type}
-            className={`${styles[color]} box-border border border-transparent rounded-md p-1 text-md w-full hover:cursor-pointer`}
+            className={`${styles[color]} box-border border border-transparent rounded-md p-1 text-md w-full hover:cursor-pointer hover:scale-110 transition-transform`}
         >
             {children}
         </button>
