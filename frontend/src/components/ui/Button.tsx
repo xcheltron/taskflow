fd24a8dcc59ef0ea0d85ce2d22a72427e46301e7
Xchel Uriel Pérez González <L20290857@cdguzman.tecnm.mx>
2026-07-29 16:36:13 -0600

    update: se actualizo el componente del boton

--- frontend/src/components/ui/Button.tsx
+++ frontend/src/components/ui/Button.tsx
@@ -1,25 +1,44 @@
 export interface ButtonProps {
     children: React.ReactNode
     type?: "button" | "submit" | "reset"
-    color?: "ok" | "disable" | "delete"
+    color?: "ok" | "disable" | "delete" | "transparent"
+    fullWidth?: boolean
+    onClick?: React.MouseEventHandler<HTMLButtonElement>
 }
 
 function Button({
     children,
     type = "button",
-    color = "ok"
+    color = "ok",
+    fullWidth = true,
+    onClick
 }: ButtonProps) {
 
     const styles = {
-        ok: "bg-blue-600 text-white",
-        disable: "bg-gray-200 text-black",
-        delete: "bg-red-600 text-white"
+        ok: "bg-blue-600 text-white hover:bg-blue-800",
+        disable: "bg-gray-200 text-black hover:bg-gray-400",
+        delete: "bg-red-600 text-white hover:bg-red-800",
+        transparent: "bg-transparen" 
     }
 
     return (
         <button 
             type={type}
-            className={`${styles[color]} box-border border border-transparent rounded-md p-1 text-md w-full hover:cursor-pointer hover:scale-110 transition-transform`}
+            onClick={onClick}
+            className={`
+                ${styles[color]}
+                ${fullWidth ? "w-full" : "w-auto"} 
+                box-border 
+                border 
+                border-transparent 
+                rounded-md 
+                px-4
+                py-2 
+                text-md
+                transition-all
+                duration-200 
+                hover:cursor-pointer 
+                hover:scale-105`}
         >
             {children}
         </button>
