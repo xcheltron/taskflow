a278bcc977f24e81757a12b10aedfa8c45c8c432
Xchel Uriel Pérez González <L20290857@cdguzman.tecnm.mx>
2026-07-30 14:48:58 -0600

    update: se actualizo el register para mandar al login directamente despues de registar

--- frontend/src/features/auth/components/RegisterForm.tsx
+++ frontend/src/features/auth/components/RegisterForm.tsx
@@ -1,22 +1,53 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate  } from "react-router-dom"
+import { useState,} from "react"
+
 import Button from "../../../components/ui/Button"
 import Input from "../../../components/ui/Input"
 
+import { register } from "../services/authService"
+
 function RegisterForm () {
+
+    const navigate = useNavigate()
+
+
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const handleSubmit = async (e: React.FormEvent) =>{
+        e.preventDefault()
+
+        try {
+            const response = await register({
+                name,
+                email,
+                password
+            })
+
+            console.log(response)
+
+            navigate('/')
+
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <h5 className="text-xl font-semibold text-heading mb-6">Register</h5>
                 <div className="mb-4">
                     <label htmlFor="name" className="block mb-1 text-md font-mono text-heading">Name</label>
-                    <Input id="name" name="name" type="text" placeholder="Juan Perez"></Input>
+                    <Input id="name" name="name" type="text" placeholder="Juan Perez" value={name} onChange={(e) => setName(e.target.value)}></Input>
                 </div>
                 <div className="mb-4">
                     <label htmlFor="email" className="block mb-1 text-md font-mono text-heading">Your email</label>
-                    <Input id="email" name="email" placeholder="example@email.com"></Input>
+                    <Input id="email" name="email" placeholder="example@email.com" value={email} onChange={(e) => setEmail(e.target.value)}></Input>
                 </div>
                 <div>
                     <label htmlFor="password" className="block mb-1 text-md font-mono text-heading">Your password</label>
-                    <Input id="password" name="password" type="password" placeholder="*******"></Input>
+                    <Input id="password" name="password" type="password" placeholder="*******" value={password} onChange={(e) => setPassword(e.target.value)}></Input>
                 </div>
                 <div className="pt-6">
                     <Button type="submit" color="ok">Register</Button>
