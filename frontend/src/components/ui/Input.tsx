464d8fce92b8a7716b2f9770c37720e695333b22
Xchel Uriel Pérez González <L20290857@cdguzman.tecnm.mx>
2026-07-30 14:11:47 -0600

    update: se actualizo el componente Input

--- frontend/src/components/ui/Input.tsx
+++ frontend/src/components/ui/Input.tsx
@@ -1,19 +1,30 @@
+import type { ChangeEvent } from "react"
+
 export interface InputProps {
     id: string,
     name: string, 
     placeholder?: string
     type?: "email" | "password" | "text" | "checkbox"
+    value?: string
+    onChange?: (e: ChangeEvent<HTMLInputElement>) => void 
 }
 
 function Input({
+    id,
+    name,
     placeholder,
-    type = "email"
+    type = "email",
+    value,
+    onChange
 }: InputProps) {
-
     return (
         <input
+            id={id}
+            name={name}
             type={type}
             placeholder={placeholder}
+            value={value}
+            onChange={onChange}
             className="bg-neutral-100 border rounded-md text-sm block w-full px-3 py-2.5 shadow-xs placeholder:text-body pb-1"
             required
         />
