ccac95017f98d2af2465fcf0a735e06e31034c1a
Xchel Uriel Pérez González <L20290857@cdguzman.tecnm.mx>
2026-07-31 13:52:09 -0600

    Update: se actualizo el project service

--- frontend/src/features/projects/services/projectService.ts
+++ frontend/src/features/projects/services/projectService.ts
@@ -1,8 +1,12 @@
 import api from "../../../api/axios";
-import type { countProjectData } from "../types";
+import type { countProjectData, getProjectsTypes } from "../types";
 
 export const countProjects = async (data: countProjectData)=> {
     const response = await api.get(`projects/countProjects/${data.id}`)
     return response.data
 }
 
+export const getProjects = async (data: getProjectsTypes) => {
+    const response = await api.get(`projects/user/${data.id}`)
+    return response.data
+}
