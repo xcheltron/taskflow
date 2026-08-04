97c8115dfde636f256d6bede8d6725bd3ffa68a0
Xchel Uriel Pérez González <L20290857@cdguzman.tecnm.mx>
2026-08-04 16:04:24 -0600

    Update: se grago la nueva ruta para poner dashboard

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,7 @@ import cors from "cors"
 import userRoutes from "./routes/user.routs.js";
 import projectRoutes from "./routes/project.routs.js"
 import taskRoutes from "./routes/task.routs.js"
+import dashBoard from "./routes/dashBoard.routs.js"
 
 const app = express();
 
@@ -20,5 +21,6 @@ app.get("/", (req,res) =>{
 app.use("/api/users", userRoutes);
 app.use("/api/projects", projectRoutes)
 app.use("/api/tasks", taskRoutes)
+app.use("/api/dashboard", dashBoard)
 
 export default app;
