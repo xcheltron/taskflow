49fe8158c1e4dabe11f523e307367d3d07624ce0
Xchel Uriel Pérez González <L20290857@cdguzman.tecnm.mx>
2026-08-06 17:05:04 -0600

    Se agrego el servicio para agregar una tarea nueva

--- frontend/src/features/tasks/services/taskService.ts
+++ frontend/src/features/tasks/services/taskService.ts
@@ -1,7 +1,12 @@
 import api from "../../../api/axios";
-import type { getTasksData } from "../types";
+import type { getTasksData, createTaskData } from "../types";
 
 export const getTasks = async (data: getTasksData) => {
     const response = await api.get(`tasks/find/${data.id}`)
     return response.data
+}
+
+export const createTask = async (data:createTaskData)=> {
+    const responese = await api.post(`tasks/create`, data)
+    return responese.data
 }
