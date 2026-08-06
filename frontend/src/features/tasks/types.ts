15b5c157ab3abb8ca876c135233de86430a002a5
Xchel Uriel Pérez González <L20290857@cdguzman.tecnm.mx>
2026-08-06 17:05:33 -0600

    Se agrego el tipo creteTask

--- frontend/src/features/tasks/types.ts
+++ frontend/src/features/tasks/types.ts
@@ -1,3 +1,21 @@
 export interface getTasksData{
     id:number
 }
+
+export interface task{
+    id: number
+    title: string
+    description: string | null
+    status: "COMPLETED" | "PENDING"
+    priority: "LOW" | "MEDIUM" | "HIGH"
+    duedate: string
+}
+
+export interface createTaskData{
+    title: string
+    description: string | null
+    status: "PENDING"
+    priority: "LOW" | "MEDIUM" | "HIGH"
+    duedate: Date
+    projectId: number
+}
