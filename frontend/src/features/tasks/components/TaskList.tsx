ac63e8a0e66dfb5f7ee2d8441c25d254ecbe8801
Xchel Uriel Pérez González <L20290857@cdguzman.tecnm.mx>
2026-08-06 17:03:57 -0600

    Se separo el componente lista a un nuevo archivo

---
+++ frontend/src/features/tasks/components/TaskList.tsx
@@ -0,0 +1,50 @@
+import Button from "../../../components/ui/Button"
+
+export interface taskListProps {
+    title: string
+    priority: "LOW" | "MEDIUM" | "HIGH"
+    description?: string | null
+
+}
+
+function TaskList ({
+    title,
+    description,
+    priority
+}:taskListProps) {
+    
+    const priorityColor = {
+        LOW: "bg-green-500",    
+        MEDIUM: "bg-amber-400",
+        HIGH: "bg-red-500",
+    }
+    return(
+        <div className="flex flex-col gap-2">
+            <div className="flex items-center justify-between border-b py-4">
+                <div className="flex items-center gap-4">
+                    <input type="checkbox" className="mt-1 size-6" />
+                        <div>
+                            <label className="text-xl">
+                                {title}
+                            </label>
+                                {description && (
+                            <p className="mt-1 text-sm text-gray-500">
+                                {description}
+                            </p>
+                                )}
+                        </div>
+                </div>
+
+                <div className="flex items-center gap-4">
+                    <div className={`size-4 rounded-full ${priorityColor[priority]}`} />
+
+                        <Button id="basura" color="transparent" fullWidth={false}>
+                            🗑
+                        </Button>
+                    </div>
+                </div>
+        </div>
+    )
+}
+
+export default TaskList
