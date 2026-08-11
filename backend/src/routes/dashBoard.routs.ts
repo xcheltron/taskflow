af08369950065cfae5c30703aa2c40894e645c3f
Xchel Uriel Pérez González <L20290857@cdguzman.tecnm.mx>
2026-08-11 16:13:21 -0600

    Se agrego la ruta para consultar el dash board

--- backend/src/routes/dashBoard.routs.ts
+++ backend/src/routes/dashBoard.routs.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 
-import { getTotalTaskController } from "../controllers/dashBoard.controller.js";
+import { getTotalTaskController, projectCountController } from "../controllers/dashBoard.controller.js";
 
 const router = Router()
 
+router.get('/countProjects/:id', projectCountController)
 router.get('/getTasks/:id', getTotalTaskController)
 
 export default router
