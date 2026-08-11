727f8f0f41b81fbe8fff56072c9a414bad270e34
Xchel Uriel Pérez González <L20290857@cdguzman.tecnm.mx>
2026-08-11 16:12:52 -0600

    Se agrego el modelo para contar proyectos

--- backend/src/models/dashBoard.model.ts
+++ backend/src/models/dashBoard.model.ts
@@ -1,6 +1,14 @@
 import prisma from "../config/prisma.js";
 import { TaskStatus } from "../generated/prisma/enums.js";
 
+//traer la cantidad de pryectos que tiene un usuario
+export const projectCountModel = async (userId:number) => {
+    return await prisma.user.findMany({
+        where: {id: userId},
+        select: {_count: true}
+    })
+}
+
 export const getTotalTask = async (
     userId: number
 ) => {
