1a6e813f9e17061a5d71f126d540ccda9513f772
Xchel Uriel Pérez González <L20290857@cdguzman.tecnm.mx>
2026-08-11 16:12:12 -0600

    Se agrego el controlador de contarp pryectos

--- backend/src/controllers/dashBoard.controller.ts
+++ backend/src/controllers/dashBoard.controller.ts
@@ -1,6 +1,33 @@
 import type { Request, Response } from "express";
 
-import { getTotalTask } from "../models/dashBoard.model.js";
+import { getTotalTask, projectCountModel } from "../models/dashBoard.model.js";
+
+
+export const projectCountController = async (
+    req: Request,
+    res: Response
+) => {
+    try {
+        const userId = Number(req.params.id)
+
+        if (isNaN(userId)){
+            return res.status(400).json({
+                message: "id no valido"
+            })
+        }
+
+        const result = await projectCountModel(userId)
+
+        return res.status(200).json({
+            message: "Conteo realizado con exito",
+            result: result
+        })
+
+    } catch (error) {
+        console.error(error)
+        return res.status(500).json({message: "Error interno del servidor"})
+    }
+}
 
 export const getTotalTaskController = async(
     req: Request,
