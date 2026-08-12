23887eaf7489a42ecd791951297d7c88f988b185
Xchel Uriel Pérez González <L20290857@cdguzman.tecnm.mx>
2026-08-11 18:31:40 -0600

    Se actualizo el presptador de servicio para agregar la funcion eliminar tarea

--- frontend/src/features/tasks/services/taskService.ts
+++ frontend/src/features/tasks/services/taskService.ts
@@ -1,5 +1,5 @@
 import api from "../../../api/axios";
-import type { getTasksData, createTaskData } from "../types";
+import type { getTasksData, createTaskData, deleteTasksData } from "../types";
 
 export const getTasks = async (data: getTasksData) => {
     const response = await api.get(`tasks/find/${data.id}`)
@@ -9,4 +9,9 @@ export const getTasks = async (data: getTasksData) => {
 export const createTask = async (data:createTaskData)=> {
     const responese = await api.post(`tasks/create`, data)
     return responese.data
+}
+
+export const deleteTask = async (data: deleteTasksData)=> {
+    const response = await api.delete(`tasks/delete/${data.id}`)
+    return response.data
 }
