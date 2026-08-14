4ab400ff3c7b15439420520953098630b3343b5d
Xchel Uriel Pérez González <L20290857@cdguzman.tecnm.mx>
2026-08-14 10:46:30 -0600

    Se movio projectform a la carpeta de projects

--- frontend/src/features/projects/components/ProjectForm.tsx
+++ frontend/src/features/projects/components/ProjectForm.tsx
@@ -1,6 +1,6 @@
 import Card from "../../../components/ui/Card"
 import Button from "../../../components/ui/Button"
-import { getUser } from "../storage"
+import { getUser } from "../../auth/storage"
 import { useEffect, useState } from "react"
 import { getProjects } from "../../projects/services/projectService"
 import { useNavigate } from "react-router-dom"
